291d8a4a7143c69a9823e5fa46256aef3b19b4be
Hassan-Jamal <hassanjamal6577987@gmail.com>
2023-12-26 20:28:57 +0500

    changes occurs in quote

--- src/components/Quote.jsx
+++ src/components/Quote.jsx
@@ -4,11 +4,7 @@ const Quote = () => {
   return (
         <div className="quotes">
           <blockquote cite="https://random-math-quote-api.herokuapp.com/">
-            Wherever there is number, there is beauty.-- Proclus
-          </blockquote>
-          <blockquote cite="https://random-math-quote-api.herokuapp.com/">
-            Mathematics is not about numbers, equations, computations, or algorithms:
-            it is about understanding.-- William Paul Thurston
+          "Do not worry too much about your difficulty in mathematics; I can assure you that mine are still greater." - Albert Einstein
           </blockquote>
         </div>
       );
