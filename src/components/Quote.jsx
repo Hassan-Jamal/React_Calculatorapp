c61a3a40326cf6ae9691c022a605a0c216f91e0d
Hassan-Jamal <hassanjamal6577987@gmail.com>
2023-12-26 20:34:39 +0500

    changes occurs in quote

--- src/components/Quote.jsx
+++ src/components/Quote.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Quote = () => {
   return (
         <div className="quotes">
-          <blockquote cite="https://random-math-quote-api.herokuapp.com/">
+          <blockquote>
           "Do not worry too much about your difficulty in mathematics; I can assure you that mine are still greater." - Albert Einstein
           </blockquote>
         </div>
